6855ef261af80e509004fd3d87cd558e1440da8c
Kirill <info@sampx.ru>
2021-08-01 20:51:14 +0300

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        String currentDate = item.getCreated().format(DateTimeFormatter
+                .ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss"));
+        System.out.println(currentDate);
+    }
+}
