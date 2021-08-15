7fb2b8417597f03f1b74df6af6512c65e935141c
Kirill <info@sampx.ru>
2021-08-15 12:15:50 +0300

    2.2. Реализация класса StartUI. Вывод меню [#500743 #189272]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -21,39 +21,33 @@ public class StartUI {
             } else {
                 run = false;
             }
-            switch (select) {
-                case (0):
-                    System.out.println("Введите новую задачу");
-                    String item = scanner.nextLine();
-                    tracker.add(new Item(item));
-                    break;
-                case (1):
-                    Item[] list = tracker.findAll();
-                    for (Item item1 : list) {
-                        System.out.println(item1);
-                        break;
-                    }
-                case (2):
-                    System.out.println("Введите ид задания:");
-                    int replaceId = Integer.parseInt(scanner.nextLine());
-                    System.out.println("Введите новое имя дела");
-                    String replaceName = scanner.nextLine();
-                    tracker.replace(replaceId, new Item(replaceName));
-                    break;
-                case (3):
-                    System.out.println("Введите ид задания которое требуется удалить");
-                    int delId = scanner.nextInt();
-                    System.out.println(tracker.delete(delId) ? "Дело удалено" : "Ид не найден");
-                    break;
-                case (4):
-                    System.out.println("Введите ид задания которое требуется найти");
-                    int findId = Integer.parseInt(scanner.nextLine());
-                    System.out.println(tracker.findById(findId));
-                    break;
-                case (5):
-                    System.out.println("Введите имя задания которое требуется найти");
-                    String findName = scanner.nextLine();
-                    System.out.println(tracker.findByName(findName));
+            if (select == 0) {
+                System.out.println("Введите новую задачу");
+                String item = scanner.nextLine();
+                tracker.add(new Item(item));
+            } else if (select == 1) {
+                Item[] list = tracker.findAll();
+                for (Item item1 : list) {
+                    System.out.println(item1);
+                }
+            } else if (select == 2) {
+                System.out.println("Введите ид задания:");
+                int replaceId = Integer.parseInt(scanner.nextLine());
+                System.out.println("Введите новое имя дела");
+                String replaceName = scanner.nextLine();
+                tracker.replace(replaceId, new Item(replaceName));
+            } else if (select == 3) {
+                System.out.println("Введите ид задания которое требуется удалить");
+                int delId = scanner.nextInt();
+                System.out.println(tracker.delete(delId) ? "Дело удалено" : "Ид не найден");
+            } else if (select == 4) {
+                System.out.println("Введите ид задания которое требуется найти");
+                int findId = Integer.parseInt(scanner.nextLine());
+                System.out.println(tracker.findById(findId));
+            } else if (select == 5) {
+                System.out.println("Введите имя задания которое требуется найти");
+                String findName = scanner.nextLine();
+                System.out.println(tracker.findByName(findName));
             }
         }
     }
