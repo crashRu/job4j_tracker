0b8a392c5c23289b67f125de336c1618d0100fdd
crash <info@sampx.ru>
2021-08-25 11:14:27 +0300

    5.1. Тестирование. Подготовка данных. [#182960]

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+
+public class StartUITest {
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "replaced item"};
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("First item");
+        tracker.add(item);
+        int id = item.getId();
+        tracker.delete(id);
+        Assert.assertThat(tracker.findById(id), Matchers.is(nullValue()));
+
+    }
+}
