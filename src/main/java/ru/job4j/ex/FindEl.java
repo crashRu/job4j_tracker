785c602c56f2048d85972aab84c9703c71c3690b
Kirill <info@sampx.ru>
2021-09-11 17:18:23 +0300

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,6 +6,7 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rls = i;
+                break;
             }
         }
         if (rls == -1) {
