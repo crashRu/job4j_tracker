6b33a6f9d1e7300c19e1310c6dfd10087a59b2bc
crash <info@sampx.ru>
2021-10-17 23:20:16 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+public class UniqueText {
+    public static void main(String[] args) {
+        String origin = "My cat eats a mouse and milk";
+        String text = "My cat eats milk and a mouse";
+
+        System.out.println(isEquals(origin, text));
+
+        String origin2 = "My cat eats a mouse";
+        String text2 = "A mouse is eaten by a cat";
+
+        System.out.println(isEquals(origin2, text2));
+    }
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        check.addAll(List.of(origin));
+        for (String s : text) {
+            rsl = check.contains(s);
+            }
+        return rsl;
+    }
+}
