e583039c15888d145126e9569903ed96055f6952
Kirill <info@sampx.ru>
2021-11-03 18:47:38 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/hashMap/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection.hashMap;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("info@sampx.ru", "Kirill Antokhi");
+        map.put("info@inn-home.ru", "Antokhi Kirill");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println("Ключ: " + key + " value: " + value);
+        }
+    }
+}
