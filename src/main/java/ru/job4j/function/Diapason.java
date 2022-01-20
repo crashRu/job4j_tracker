71ace54856c6bbf25feb2f4139742dcc481dbb4e
Kirill <info@sampx.ru>
2022-01-20 10:22:27 +0300

    6. Функции высшего порядка [#24255]

---
+++ src/main/java/ru/job4j/function/Diapason.java
@@ -0,0 +1,10 @@
+package ru.job4j.function;
+
+import java.util.function.Function;
+
+public class Diapason {
+
+    public static void main(String[] args) {
+        Function<Double, Double> function = aDouble -> aDouble * 2;
+    }
+}
