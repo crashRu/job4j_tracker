47a4a2b4d668ba686b93271964a6f1fe630ded37
Kirill <info@sampx.ru>
2022-01-23 18:13:15 +0300

    4. Преобразование List в Map. [#110226]

--- src/main/java/ru/job4j/stream/Student.java
+++ src/main/java/ru/job4j/stream/Student.java
@@ -1,5 +1,7 @@
 package ru.job4j.stream;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class Student {
@@ -23,18 +25,19 @@ public class Student {
     @Override
     public boolean equals(Object o) {
         if (this == o) {
+
             return true;
         }
         if (o == null || getClass() != o.getClass()) {
+
             return false;
         }
         Student student = (Student) o;
-        return score == student.score
-                && Objects.equals(surname, student.surname);
+        return Objects.equals(surname, student.surname);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(score, surname);
+        return Objects.hash(surname);
     }
 }
