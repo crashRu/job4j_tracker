ba66943668e165300f0ae5f540fbda52dc3f8f72
Kirill <info@sampx.ru>
2022-01-23 20:03:20 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,19 +1,22 @@
 package ru.job4j.bank;
 
 import java.util.*;
+
 /**
- *  The class describes the operation of bank services
+ * The class describes the operation of bank services
+ *
  * @author Kirill Antokhii
  * @version 1.0
  */
 public class BankService {
     /**
-     *  Data storage is carried out in a collection of the HashMap type
+     * Data storage is carried out in a collection of the HashMap type
      */
     private final Map<User, List<Account>> users = new HashMap<>();
 
     /**
      * The method adds a new user to the system
+     *
      * @param user
      */
     public void addUser(User user) {
@@ -21,11 +24,12 @@ public class BankService {
     }
 
     /**
-     *The method adds a new account to the user,
-     *  if the user does not find the account in
-     *  the system, it will not be added
+     * The method adds a new account to the user,
+     * if the user does not find the account in
+     * the system, it will not be added
+     *
      * @param passport - Passport ID
-     * @param account - Account number to add to the system
+     * @param account  - Account number to add to the system
      */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -37,62 +41,58 @@ public class BankService {
     /**
      * The method searches for a person in
      * the system using a passport
+     *
      * @param passport -Passport ID
      * @return -Returns the found user
      * if the user is not found returns null
      */
     public User findByPassport(String passport) {
-        User searchUser = null;
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                searchUser = user;
-                break;
-            }
-        }
-        return searchUser;
+        return users.keySet()
+                .stream()
+                .filter(u -> u.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
-    /**
-     * The method searches for the user's details in the system,
-     * the search is performed by iterating over all users,
-     * returns the user, if the user is not found returns null
-     * @param passport - Passport ID
-     * @param requisite - User details
-     * @return Account or null
-     */
-    public Account findByRequisite(String passport, String requisite) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            List<Account> account = users.get(user);
-            for (Account acc : account) {
-                if (requisite.equals(acc.getRequisite())) {
-                    return acc;
-                }
+        /**
+         * The method searches for the user's details in the system,
+         * the search is performed by iterating over all users,
+         * returns the user, if the user is not found returns null
+         * @param passport - Passport ID
+         * @param requisite - User details
+         * @return Account or null
+         */
+        public Account findByRequisite(String passport, String requisite) {
+            User user = findByPassport(passport);
+            if (user != null) {
+                return users.get(user).stream()
+                        .filter(acc -> requisite.equals(acc.getRequisite()))
+                        .findFirst()
+                        .orElse(null);
             }
+            return null;
         }
-        return null;
-    }
+        /**
+         *The method searches for users as well as an account
+         * for transfer, checks whether all conditions are suitable for transfer
+         * @param srcPassport - Original passpotr
+         * @param srcRequisite - Original requisite
+         * @param destPassport - Account for transfer
+         * @param destRequisite - transfer details
+         * @param amount - price transfer
+         * @return - Transfer status
+         */
 
-    /**
-     *The method searches for users as well as an account
-     * for transfer, checks whether all conditions are suitable for transfer
-     * @param srcPassport - Original passpotr
-     * @param srcRequisite - Original requisite
-     * @param destPassport - Account for transfer
-     * @param destRequisite - transfer details
-     * @param amount - price transfer
-     * @return - Transfer status
-     */
-    public boolean transferMoney(String srcPassport, String srcRequisite,
-                                 String destPassport, String destRequisite, double amount) {
-        boolean rsl = false;
-        Account src = findByRequisite(srcPassport, srcRequisite);
-        Account dest = findByRequisite(destPassport, destRequisite);
-        if (src != null && dest != null && src.getBalance() >= amount) {
-            src.setBalance(src.getBalance() - amount);
-            dest.setBalance(dest.getBalance() + amount);
-            rsl = true;
+        public boolean transferMoney(String srcPassport, String srcRequisite,
+                String destPassport, String destRequisite, double amount) {
+            boolean rsl = false;
+            Account src = findByRequisite(srcPassport, srcRequisite);
+            Account dest = findByRequisite(destPassport, destRequisite);
+            if (src != null && dest != null && src.getBalance() >= amount) {
+                src.setBalance(src.getBalance() - amount);
+                dest.setBalance(dest.getBalance() + amount);
+                rsl = true;
+            }
+            return rsl;
         }
-        return rsl;
     }
-}
